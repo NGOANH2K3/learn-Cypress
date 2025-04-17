9b49619b142eea9652a1972c864764ea5797cca4
NGOANH2K3 <ngo24395@gmail.com>
2025-04-17 17:35:30 +0700

    add day3 Execute test in PARALLEL with sorry-cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
-
+const { cloudPlugin } = require("cypress-cloud/plugin");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return cloudPlugin(on, config);
     },
     specPattern: 'cypress/api-test/**/*.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
+    projectId: 'Cypress-api',
   },
 });
